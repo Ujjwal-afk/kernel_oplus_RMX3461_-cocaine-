f1c90ab913d743d49b8de266c7081f98bfcca395
Mathias Nyman <mathias.nyman@linux.intel.com>
2023-01-16 16:22:13 +0200

    merged with 5.4.230

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -79,8 +79,16 @@ static const char hcd_name[] = "xhci_hcd";
 
 static struct hc_driver __read_mostly xhci_pci_hc_driver;
 
+<<<<<<< HEAD
+=======
+static int xhci_pci_setup(struct usb_hcd *hcd);
+static int xhci_pci_update_hub_device(struct usb_hcd *hcd, struct usb_device *hdev,
+				      struct usb_tt *tt, gfp_t mem_flags);
+
+>>>>>>> cf6e5d3c2de1 (xhci: Add update_hub_device override for PCI xHCI hosts)
 static const struct xhci_driver_overrides xhci_pci_overrides __initconst = {
 	.reset = xhci_pci_setup,
+	.update_hub_device = xhci_pci_update_hub_device,
 };
 
 /* called after powerup, by probe or system-pm "wakeup" */
@@ -363,6 +371,12 @@ int xhci_pci_setup(struct usb_hcd *hcd)
 	return xhci_pci_reinit(xhci, pdev);
 }
 EXPORT_SYMBOL_GPL(xhci_pci_setup);
+
+static int xhci_pci_update_hub_device(struct usb_hcd *hcd, struct usb_device *hdev,
+				      struct usb_tt *tt, gfp_t mem_flags)
+{
+	return xhci_update_hub_device(hcd, hdev, tt, mem_flags);
+}
 
 /*
  * We need to register our own PCI probe function (instead of the USB core's
